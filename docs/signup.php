a782eda605e0e174537e029d4b1590af69535144
Charles Annan <79229028+stiffchoco@users.noreply.github.com>
2022-01-24 11:21:02 +0000

    Update signup.php

--- docs/signup.php
+++ docs/signup.php
@@ -89,6 +89,7 @@ if (isset($_SESSION['verified_user_id']))
 				<label>Password: </label>
 				<input type="password" name="password" placeholder="Enter Password" required style="height:60% !important;">
 				<div style="font-size:10px">Note: Password must be a combination of at least 8 characters </div>
+				<div style="font-size:10px">Example: P@55w0rd</div>
 			</div>
 			
 			<div class="btn" style="margin-top: -20px;">
